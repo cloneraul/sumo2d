f6ae976fee7244e68ac94171d8b4b9823ef9cadb
Lauan Lucas Moura da Silva <lauan.lucas@escolar.ifrn.edu.br>
2026-07-10 14:15:45 -0300

    save
    finalizando

--- sumo/Assets/Scripts/DetectorQueda1.cs
+++ sumo/Assets/Scripts/DetectorQueda1.cs
@@ -4,19 +4,19 @@ public class DetectorQueda : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // Verifica se o objeto que caiu foi o Jogador 1
-        if (collision.name == "Teste_J1")
+        // Se o objeto que encostou no sensor começar com o nome "Teste_J1"
+        if (collision.name.StartsWith("Teste_J1"))
         {
-            // Se o J1 caiu, o ponto vai para o Jogador 2!
+            // O Jogador 1 caiu, então o ponto vai para o Jogador 2!
             if (GameManager.Instance != null)
             {
                 GameManager.Instance.RegistrarPonto(2);
             }
         }
-        // Verifica se o objeto que caiu foi o Jogador 2
-        else if (collision.name == "Teste_J2")
+        // Se o objeto que encostou no sensor começar com o nome "Teste_J2"
+        else if (collision.name.StartsWith("Teste_J2"))
         {
-            // Se o J2 caiu, o ponto vai para o Jogador 1!
+            // O Jogador 2 caiu, então o ponto vai para o Jogador 1!
             if (GameManager.Instance != null)
             {
                 GameManager.Instance.RegistrarPonto(1);
